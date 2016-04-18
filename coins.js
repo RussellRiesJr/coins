25395a260b374f658891e23ec442dc6c3b8cfba7
Russell Ries <riesru@gmail.com>
2016-04-18 11:32:31 -0500

    finish exercise

--- coins.js
+++ coins.js
@@ -13,15 +13,29 @@
   }
 
 */
+var money = prompt("Enter a dollar amount without dollar sign", "1.37");
 
-function coinCounter () {
+function coinCounter (cash) {
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
 
-  coinPurse.quarters = 0;
+  var cash = cash * 100;
+
+  var lessQuarters = cash % 25;
+  coinPurse.quarters = (cash - lessQuarters) / 25;
+  var afterQuarters = cash - (coinPurse.quarters * 25);
+  var lessDimes = afterQuarters % 10;
+  coinPurse.dimes = (afterQuarters - lessDimes) / 10;
+  var afterDimes = afterQuarters - (coinPurse.dimes * 10);
+  var lessNickels = afterDimes % 5;
+  coinPurse.nickels = (afterDimes - lessNickels) / 5;
+  var afterNickels = afterDimes - (coinPurse.nickels * 5);
+  var lessPennys = afterNickels % 1;
+  coinPurse.pennys = (afterNickels - lessPennys) / 1;
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+
+var coins = coinCounter(money)
+console.log(coins);
